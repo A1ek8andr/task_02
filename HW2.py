02e1c76ebf28d0478e395eb4f56ec19b85c2321c
A1ek8andr <61617590+A1ek8andr@users.noreply.github.com>
2020-03-01 20:26:15 +0300

    version 2
    Исправлены замечания

---
+++ HW2.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import requests as rqst
+import re
+import scipy.special as sp
+import os
+
+
+# извлечение данных из файла
+def task2():
+    url = 'http://www.jenyay.net/uploads/Student/Modelling/task_02.txt'
+    task = rqst.get(url)
+    z = re.search(r'4\W.+', task.text)
+    z1 = (z.group().split(';'))
+    D = float(z1[0].split('=')[1])
+    fmin = float(z1[1].split('=')[1])
+    fmax = float(z1[2].split('=')[1])
+    # открытие файла с результатами и создание заголовка
+    res = open('results/task_02_4O-506C_Podberezniy_4.txt', 'w')
+    print(
+        '      f[ГГц]    ' +
+        '            ' +
+        '      sigma[м^2]    ',
+        file=res)
+
+    # Вычислительная часть
+    c = 3e8
+    r = D / 2
+    f = np.linspace(fmin, fmax + 1, 500)
+    sigma1 = []
+    for fx in f:
+        lamda = c / fx
+        k = 2 * np.pi / lamda
+        h = []
+        b = [0]
+        a = []
+        sigmaN = []
+        n = 0
+        while n < 50:
+            h.append(
+                sp.spherical_jn(n,k * r) + 1j * sp.spherical_yn(n, k * r))
+            a.append(sp.spherical_jn(n, k * r) / h[n])
+            n += 1
+        n = 1
+        while n < 50:
+            b.append((k * r * sp.spherical_jn(n - 1, k * r) - n * \
+                     sp.spherical_jn(n, k * r)) / (k * r * h[n - 1] - n * h[n]))
+            n += 1
+        n = 1
+        while n < 50:
+            sigmaN.append(((-1)**n) * (n + 1 / 2) * (b[n] - a[n]))
+            n += 1
+        sigma = (lamda**2 / np.pi) * (abs(np.sum(sigmaN)))**2
+        print(str(fx * 1e-9) + '            ' + str(sigma), file=res)
+        sigma1.append(sigma / (np.pi * (r**2)))
+    res.close()
+
+    # Построение графика
+    plt.figure()
+    plt.plot(2 * np.pi * f * r / c, sigma1)
+    plt.grid()
+    plt.ylabel('ЭПР')
+    plt.xlabel('длина волны')
+    plt.show()
+    return()
+
+
+try:  # Если папка result и файл в ней существуют
+    os.remove('results/task_02_4O-506C_Podberezniy_4.txt')
+    os.rmdir("results")
+    os.mkdir('results')
+    task2()
+except OSError:
+    try:  # Если нет папки и файла
+        os.mkdir('results')
+        task2()
+    except OSError:  # Если нет только файла
+        task2()
